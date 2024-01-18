62f1c8a67b821325ed77578624fcd551a699a28e
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2024-01-18 13:27:13 -0800

    Add files via upload

---
+++ public/client-listenerHub.js
@@ -0,0 +1,35 @@
+const sweetVenom = document.getElementById('sweet-venom');
+const givenTaken = document.getElementById('given-taken');
+const drunkDazed = document.getElementById('drunk-dazed');
+const tamedDashed = document.getElementById('tamed-dashed');
+const blessedCursed = document.getElementById('blessed-cursed');
+const futurePerfect = document.getElementById('future-perfect'); 
+const biteMe = document.getElementById('bite-me');
+
+sweetVenom.addEventListener('click', () => {
+    window.location.href = 'https://www.youtube.com/watch?v=qedonJosQ3g&ab_channel=HYBELABELS';
+});
+
+givenTaken.addEventListener('click', () => {
+    window.location.href = 'https://youtu.be/nQ6wLuYvGd4?si=dQsOQNx83C4Dj4D6';
+});
+
+drunkDazed.addEventListener('click', () => {
+    window.location.href = 'https://youtu.be/Fc7-Oe0tj5k?si=rcaqQFj7aXhpgh6i';
+});
+
+tamedDashed.addEventListener('click', () => {
+    window.location.href = 'https://youtu.be/6IRsW_TkyVA?si=nDNiXv3Sg-OYYgyw';
+});
+
+blessedCursed.addEventListener('click', () => {
+    window.location.href = 'https://youtu.be/osmHArcf_aE?si=jh4ViqSGJ7vW3WIP';
+});
+
+futurePerfect.addEventListener('click', () => {
+    window.location.href = 'https://youtu.be/QMlNLo74mOw?si=If1qj5hT5KoyzBV4';
+});
+
+biteMe.addEventListener('click', () => {
+    window.location.href = 'https://youtu.be/wXFLzODIdUI?si=FxilOeByyACKJICh';
+});
