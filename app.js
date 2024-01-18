933b5ba7c8abe68b0c59be416a934de2b375253e
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2024-01-18 13:24:55 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const { createServer } = require('http');
 const { join } = require('path');
-const { Server } = require('socket.io');
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
 
 app.use(express.static(join(__dirname, 'public')));
 
@@ -22,26 +18,9 @@ app.get('/engene-hub', (req, res) => {
 });
 
 app.get('/listener-hub', (req, res) => {
-    res.sendFile(join(__dirname, 'listenerHub.html'))
-})
-
-io.on('connection', (socket) => {
-    console.log('welcome to enhaflix 🕶️🍿');
-
-    socket.on('userPage', () => {
-        socket.emit('userPage');
-    })
-
-    socket.on('engeneHub', () => {
-        socket.emit('engeneHub');
-    })
-
-    socket.on('listenerHub', () => {
-        socket.emit('listenerHub');
-    })
-
+    res.sendFile(join(__dirname, 'listenerHub.html'));
 });
 
-server.listen(1130, () => {
+app.listen(1130, () => {
     console.log('server running at port 1130');
 });
