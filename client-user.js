0f65ca66a950a39074ad245c8bbfa63e4da44cdb
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2024-01-18 14:00:56 -0800

    Update client-user.js

--- client-user.js
+++ client-user.js
@@ -2,9 +2,9 @@ const engene = document.getElementById('engene');
 const listener = document.getElementById('listener');
 
 engene.addEventListener('click', () => {
-    window.location.href = ('/engene-hub');
+    window.location.href = ('/enhaflix/engeneHub');
 });
 
 listener.addEventListener('click', () => {
-    window.location.href = ('/listener-hub');
+    window.location.href = ('/enhaflix/listenerHub');
 })
