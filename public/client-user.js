a24282189171a511eb2ed535d6f86306dcfe22fd
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2024-01-18 13:27:35 -0800

    Update client-user.js

--- public/client-user.js
+++ public/client-user.js
@@ -1,20 +1,10 @@
-var socket = io();
-
 const engene = document.getElementById('engene');
 const listener = document.getElementById('listener');
 
 engene.addEventListener('click', () => {
-    socket.emit('engeneHub');
-});
-
-listener.addEventListener('click', () => {
-    socket.emit('listenerHub');
-})
-
-socket.on('engeneHub', () => {
     window.location.href = ('/engene-hub');
 });
 
-socket.on('listenerHub', () => {
-    window.location.href = ('listener-hub');
-});
+listener.addEventListener('click', () => {
+    window.location.href = ('/listener-hub');
+})
