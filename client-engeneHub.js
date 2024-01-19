55ba4000c44dbe4f2188730c836aa1da33764e07
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2024-01-18 20:04:14 -0800

    Update client-engeneHub.js

--- client-engeneHub.js
+++ client-engeneHub.js
@@ -1,8 +1,12 @@
-const sweetVenom = document.getElementById('sweet-venom');
+const sweetVenom = document.getElementById('header');
 const iland = document.getElementById('iland');
 const enoclock = document.getElementById('enoclock');
 const gameCaterers = document.getElementById('game-caterers');
 const sosofun = document.getElementById('sosofun');
+
+const note = document.getElementById('note');
+const enlog = document.getElementById('enlog');
+const episode = document.getElementById('episode');
 
 sweetVenom.addEventListener('click', () => {
     window.location.href = 'https://www.youtube.com/watch?v=qedonJosQ3g&ab_channel=HYBELABELS';
@@ -22,4 +26,16 @@ gameCaterers.addEventListener('click', () => {
 
 sosofun.addEventListener('click', () => {
     window.location.href = 'https://www.youtube.com/playlist?list=PLMQ4WMltba-GrMZTqRZinxleeALwB6-YV';
+});
+
+note.addEventListener('click', () => {
+    window.location.href = 'https://www.youtube.com/playlist?list=PLMQ4WMltba-Erb1lLXpUbc5VVslavAZsi';
+});
+
+enlog.addEventListener('click', () => {
+    window.location.href = 'https://www.youtube.com/playlist?list=PLMQ4WMltba-F67iE1rTXm58n5mnFWqSir';
+});
+
+episode.addEventListener('click', () => {
+    window.location.href = 'https://www.youtube.com/playlist?list=PLMQ4WMltba-GGgaK_O288AEWD3UpWUnkc';
 });
